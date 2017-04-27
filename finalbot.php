962c95459161a2051ef5adeea4b30c82c22e7006
joelqy <joelquaye1@gmail.com>
2017-04-27 20:26:13 +0000

    Update finalbot.php

--- finalbot.php
+++ finalbot.php
@@ -2,7 +2,7 @@
 
 $botToken = "255649217:AAFxloiulIMAFEQZ29EgEn9kjyNeQ8MS9lc";
 $website = "https://api.telegram.org/bot".$botToken;
-
+$website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents($website."/getupdates");
 
 $updateArray = json_decode($update, TRUE);
